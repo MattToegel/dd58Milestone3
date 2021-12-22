b23b62a9b255633930f22ffdbf66fd68559279cb
dd58 <85141819+dd58@users.noreply.github.com>
2021-12-22 16:58:51 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'mlstn1_dp');
+define('DB_SERVER', 'db.ethereallab.app:3306');
+define('DB_USERNAME', 'dd58');
+define('DB_PASSWORD', 'mg7vRP2EyvGA');
+define('DB_NAME', 'dd58');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
